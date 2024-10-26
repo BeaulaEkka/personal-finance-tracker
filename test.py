e0bbaa780d341dfb58eae961d17ee8c7d7d0a63e
Your Name <ekka.beaula@gmail.com>
2024-10-26 22:29:27 +0200

    Microwave Class

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
 class Microwave:
-    ...
+    def __init__(self, brand, power_rating):
+        self.brand = brand
+        self.power_rating = power_rating
+        self.turned_on = False
+        
+
+    def turn_on(self):
+        if self.turned_on:
+            print(f'Microwave {self.brand} is already turned on.')
+        else:
+            self.turned_on = True
+            print(f'Microwave {self.brand} is turned on.')
+
+    def turn_off(self):
+        if self.turned_on:
+            self.turned_on=False
+            print(f'Microwave {self.brand} is now turned off.')
+        else:
+            self.turned_off = True
+
+    def run(self,seconds):
+        if self.turned_on:
+            print(f'Running {self.brand} for {seconds} seconds.')
+        else:
+            print(f'please turn on your microwave first...')
 
 
-smeg = Microwave()
-print(smeg)
+smeg = Microwave('canon', 15)
+smeg.turn_on()
+
+
+smeg.run(45)
+smeg.turn_off()
